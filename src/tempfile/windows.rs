1ab59fd288dcfaaae3a2ad80f691f4678d491abc
Iku Iwasa <iku.iwasa@gmail.com>
2021-02-23 10:39:37 +0900

    Remove unnecessary Result wrap

--- src/tempfile/windows.rs
+++ src/tempfile/windows.rs
@@ -9,17 +9,17 @@ use crate::rand::gen_random_alphanum;
 use crate::tempfile::common::TempPath;
 
 pub fn create_temp_file(dir: &Path) -> Result<(TempPath, File), Error> {
-    let temp_path = create_temp_path(dir)?;
+    let temp_path = create_temp_path(dir);
     let handle = create_file_handle(&temp_path)?;
     Ok((TempPath::new(temp_path), unsafe {
         File::from_raw_handle(handle.cast())
     }))
 }
 
-fn create_temp_path(dir: &Path) -> Result<PathBuf, Error> {
+fn create_temp_path(dir: &Path) -> PathBuf {
     let mut pb = dir.to_path_buf();
     let mut name = String::from("aikot-");
     name.push_str(&gen_random_alphanum(6));
     pb.push(name);
-    Ok(pb)
+    pb
 }
