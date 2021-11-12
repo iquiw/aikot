4c86311c0f3ed40446626fe11d746a755a50fd35
Iku Iwasa <iku.iwasa@gmail.com>
2021-11-12 23:49:22 +0900

    Adapt to windows 0.26

--- src/clipboard/windows.rs
+++ src/clipboard/windows.rs
@@ -5,9 +5,8 @@ use std::time::Duration;
 
 use anyhow::{anyhow, Error};
 
-::windows::include_bindings!();
-
-use Windows::ApplicationModel::DataTransfer::{Clipboard, ClipboardContentOptions, DataPackage};
+use windows::runtime::{Result, HSTRING};
+use windows::ApplicationModel::DataTransfer::{Clipboard, ClipboardContentOptions, DataPackage};
 
 pub fn set_clip(text: &str) -> std::result::Result<(), Error> {
     if let Ok(exe) = current_exe() {
@@ -16,12 +15,12 @@ pub fn set_clip(text: &str) -> std::result::Result<(), Error> {
     set_clip_win(text).map_err(|e| anyhow!("{}", e.message()))
 }
 
-fn set_clip_win(text: &str) -> windows::Result<()> {
+fn set_clip_win(text: &str) -> Result<()> {
     let cco = ClipboardContentOptions::new()?;
     cco.SetIsAllowedInHistory(false)?;
     cco.SetIsRoamable(false)?;
     let dp = DataPackage::new()?;
-    dp.SetText(text)?;
+    dp.SetText(HSTRING::from(text))?;
     Clipboard::SetContentWithOptions(dp, cco)?;
     Clipboard::Flush()?;
     Ok(())
@@ -29,5 +28,5 @@ fn set_clip_win(text: &str) -> windows::Result<()> {
 
 pub fn clear_clip() -> std::result::Result<(), Error> {
     sleep(Duration::from_secs(45));
-    Ok(Clipboard::Clear()?)
+    Clipboard::Clear().map_err(|e| anyhow!("{}", e.message()))
 }
