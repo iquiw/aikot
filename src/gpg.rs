6925adae0ca9f053d6a2732d556ac31ca8b5256d
Iku Iwasa <iku.iwasa@gmail.com>
2023-01-21 17:47:21 +0900

    Output to-be-encrypted contents if gpg encryption failed

--- src/gpg.rs
+++ src/gpg.rs
@@ -28,17 +28,37 @@ pub fn encrypt<P>(aikot_env: &AikotEnv, path: P, contents: &str) -> Result<(), E
 where
     P: AsRef<Path>,
 {
+    let result = encrypt_internal(aikot_env, path, contents);
+    if result.is_err() {
+        eprintln!("To-be-encrypted contents:\n-----\n{}-----", contents);
+    }
+    result
+}
+
+fn encrypt_internal<P>(aikot_env: &AikotEnv, path: P, contents: &str) -> Result<(), Error>
+where
+    P: AsRef<Path>,
+{
     let recipients = aikot_env.get_recipients()?;
     let mut cmd = gpg_common(aikot_env.gpg_path());
     cmd.stdin(Stdio::piped())
+        .stderr(Stdio::piped())
         .arg("--encrypt")
         .arg("-o")
         .arg(path.as_ref());
     for recipient in &recipients {
         cmd.arg("-r").arg(recipient);
     }
-    let child = cmd.spawn()?;
-    Ok(child.stdin.unwrap().write_all(contents.as_bytes())?)
+    let mut child = cmd.spawn()?;
+    child.stdin.take().unwrap().write_all(contents.as_bytes())?;
+    let output = child.wait_with_output()?;
+    if output.status.success() {
+        Ok(())
+    } else {
+        Err(AikotError::CommandFail {
+            stderr: String::from_utf8(output.stderr)?,
+        }.into())
+    }
 }
 
 fn gpg_common(gpg_path: &Path) -> Command {
